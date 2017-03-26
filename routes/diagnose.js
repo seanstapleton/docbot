aab85e7073cb40aa79750498f4100f460579425a
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2017-03-25 23:36:00 -0400

    Make more models

--- routes/diagnose.js
+++ routes/diagnose.js
@@ -1,3 +1,11 @@
+var symptoms = require("../models/symptomsData.json");
+
 exports.diagnosePatient = function(req, res) {
+    symptoms.Symptoms.forEach(function(element) {
+        console.log(element.Name);
+        element.Diseases.forEach(function(disease) {
+            console.log(disease);
+        });
+    });
     res.send('POST request to the Diagnosis');
 };
