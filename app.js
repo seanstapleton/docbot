31a4051936b59518609825e95cde28ee10b5ecf1
Sean Stapleton <seanstapleton100@gmail.com>
2017-03-26 10:22:24 -0400

    stopped logging http requests

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ module.exports = function(db, passport) {
     var express = require('express');
     var path = require('path');
     var favicon = require('serve-favicon');
-    var logger = require('morgan');
+    //var logger = require('morgan');
     var cookieParser = require('cookie-parser');
     var bodyParser = require('body-parser');
     var restify = require('restify');
@@ -42,7 +42,7 @@ module.exports = function(db, passport) {
 
     //  App Configuration
     //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-    app.use(logger('dev'));
+    //app.use(logger('dev'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
