0248a69db2d72042e467a3304c2bb79f7b0dc4ce
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2017-03-26 00:03:05 -0400

    Add database lookup

--- routes/diagnose.js
+++ routes/diagnose.js
@@ -1,11 +1,42 @@
 var symptoms = require("../models/symptomsData.json");
 
 exports.diagnosePatient = function(req, res) {
-    symptoms.Symptoms.forEach(function(element) {
-        console.log(element.Name);
-        element.Diseases.forEach(function(disease) {
-            console.log(disease);
+
+    var patientSymptoms = [
+        "Headache",
+        "Fever",
+        "Red spots on mouth"
+    ];
+
+    var diseases = new Map();
+    // For each symptom
+    patientSymptoms.forEach(function(symp) {
+
+        // Find symptom in the data store
+        symptoms.Symptoms.forEach(function(element) {
+
+            if(element.Name == symp) {
+                // Map it to the disease
+                element.Diseases.forEach(function(disease) {
+                    if(!diseases.get(disease)) {
+                        diseases.set(disease, 1);
+                    } else {
+                        var oldFreq = diseases.get(disease);
+                        diseases.delete(disease);
+                        diseases.set(disease, oldFreq + 1);
+                    }
+                });
+            }
         });
     });
+
+    var mostProbableDisease = diseases.keys().next().value;
+    for(var [key, value] of diseases) {
+        if(diseases.get(mostProbableDisease) < value) {
+            mostProbableDisease = key;
+        }
+    }
+    console.log(mostProbableDisease);
+
     res.send('POST request to the Diagnosis');
 };
